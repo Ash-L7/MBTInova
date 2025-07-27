901a3003c9e0368d093ea519dc91e920b2745340
Ash <ash734ys@gmail.com>
2025-07-28 07:39:16 +0800

    Updated building placement
    building is not placed upon clicking the empty space. Instead, selector tab shows up and the building chosen by player is placed down

--- Assets/Scripts/BuildingController.cs
+++ Assets/Scripts/BuildingController.cs
@@ -8,6 +8,6 @@ public class BuildingController : MonoBehaviour
 
     public void OnBuildingButtonClicked()
     {
-        gridPlacement.SetCurrentBuilding(buildingPrefab);
+        gridPlacement.PlaceSelectedBuilding(buildingPrefab);
     }
 }
--- Assets/Scripts/GridPlacement.cs
+++ Assets/Scripts/GridPlacement.cs
@@ -4,49 +4,45 @@ using UnityEngine.SceneManagement;
 public class GridPlacement : MonoBehaviour
 {
     public float gridSize = 1;
-
-    [SerializeField] private GameObject infrastructureToPlaceDown;
-
-    public void SetCurrentBuilding(GameObject buildingPrefab)
-    {
-        infrastructureToPlaceDown = buildingPrefab;
-    }
-
     public LayerMask placementCollisionLayer;
+    public GameObject buildingSelectorTab;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private Vector2 lastClickedPosition;
+    private bool awaitingBuildingSelection = false;
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !awaitingBuildingSelection)
         {
-            PlaceItem();
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 snappedMousePos = new Vector2(Mathf.Round(mousePos.x / gridSize) * gridSize,
+                                                  Mathf.Round(mousePos.y / gridSize) * gridSize);
+
+            if (CanPlaceAtPosition(snappedMousePos))
+            {
+                lastClickedPosition = snappedMousePos;
+                awaitingBuildingSelection = true;
+                buildingSelectorTab.SetActive(true);
+            }
+            else
+            {
+                SceneManager.LoadScene(2);
+            }
         }
     }
 
-    private void PlaceItem()
+    public void PlaceSelectedBuilding(GameObject prefab)
     {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 snappedMousePos = new Vector2(Mathf.Round(mousePos.x / gridSize) * gridSize,
-                                              Mathf.Round(mousePos.y / gridSize) * gridSize);
+        if (!awaitingBuildingSelection || prefab == null)
+            return;
 
-        if (CanPlaceAtPosition(snappedMousePos))
-        {
-            GameObject placed = Instantiate(infrastructureToPlaceDown, snappedMousePos, Quaternion.identity);
-            var identifier = placed.AddComponent<BuildingIdentifier>();
-            identifier.prefabName = infrastructureToPlaceDown.name;
-            placed.tag = "Building";
+        GameObject placed = Instantiate(prefab, lastClickedPosition, Quaternion.identity);
+        var identifier = placed.AddComponent<BuildingIdentifier>();
+        identifier.prefabName = prefab.name;
+        placed.tag = "Building";
 
-        }
-        else
-        {
-            SceneManager.LoadScene(2);
-        }
+        buildingSelectorTab.SetActive(false);
+        awaitingBuildingSelection = false;
     }
 
     private bool CanPlaceAtPosition(Vector3 position)
@@ -58,31 +54,14 @@ public class GridPlacement : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-
         float size = 10f;
         for (float x = -size; x <= size; x += gridSize)
         {
-            Gizmos.DrawLine(new Vector3(x, -size, 0), new Vector3(x, size, 0));
-            Gizmos.DrawLine(new Vector3(-size, x, 0), new Vector3(size, x, 0));
-        }
-
-        if (Camera.main != null)
-        {
-            Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 snappedPos = new Vector2(
-                Mathf.Round(mouseWorld.x / gridSize) * gridSize,
-                Mathf.Round(mouseWorld.y / gridSize) * gridSize
-            );
-
-            Vector3 center = new Vector3(snappedPos.x, snappedPos.y, 0f);
-            Vector3 sizeVec = new Vector3(gridSize, gridSize, 0f);
-
-            Gizmos.color = new Color(1f, 1f, 0f, 0.4f);
-            Gizmos.DrawCube(center, sizeVec);
-
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawWireCube(center, sizeVec);
+            for (float y = -size; y <= size; y += gridSize)
+            {
+                Gizmos.DrawLine(new Vector3(x, -size, 0), new Vector3(x, size, 0));
+                Gizmos.DrawLine(new Vector3(-size, y, 0), new Vector3(size, y, 0));
+            }
         }
     }
-
 }
