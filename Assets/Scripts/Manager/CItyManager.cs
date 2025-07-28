b741c6a270cc9811ff398d033d0517a30ede71f5
chiaenn <e1375550@u.nus.edu>
2025-07-28 08:10:09 +0800

    Implement Building Resource Production with Citizen Buff System
    - Added building production logic in CityManager.UpdateProduction()
    - Integrated assigned citizens to apply buffs based on trait flags
    - Included resource type and amount in BuildingData
    - Designed buff system to adjust output based on citizen traits
    - Updated ResourceManager to reflect new resource gains
    - Added debug logs for clarity and testing

--- Assets/Scripts/Manager/CItyManager.cs
+++ Assets/Scripts/Manager/CItyManager.cs
@@ -16,7 +16,30 @@ public class CityManager : MonoBehaviour
         if (ResourceManager.Instance.SpendMoney(buildingCost))
         {
             Debug.Log($"Placing building: {type} at {position}");
-            // TODO: Add building to cityData.buildings
+
+            BuildingData newBuilding = new BuildingData();
+            newBuilding.buildingID = System.Guid.NewGuid().ToString();
+            newBuilding.type = type;
+
+            switch (type)
+            {
+                case "ScienceLab":
+                    newBuilding.domain = BuildingDomain.Science;
+                    newBuilding.baseMoneyProduction = 5;
+                    newBuilding.baseScienceProduction = 20;
+                    newBuilding.baseFoodProduction = 0;
+                    newBuilding.baseEnergyProduction = 0;
+                    break;
+                case "Farm":
+                    newBuilding.domain = BuildingDomain.Logistics;
+                    newBuilding.baseMoneyProduction = 2;
+                    newBuilding.baseScienceProduction = 0;
+                    newBuilding.baseFoodProduction = 25;
+                    newBuilding.baseEnergyProduction = 0;
+                    break;
+            }
+
+            cityData.buildings.Add(newBuilding);
         }
         else
         {
@@ -25,8 +48,28 @@ public class CityManager : MonoBehaviour
     }
 
 
+
     public void UpdateProduction()
     {
-        // TODO: Building output resources
+        foreach (var building in cityData.buildings)
+        {
+            float moneyOutput = building.baseMoneyProduction * building.productionMultiplier;
+            float scienceOutput = building.baseScienceProduction * building.productionMultiplier;
+            float foodOutput = building.baseFoodProduction * building.productionMultiplier;
+            float energyOutput = building.baseEnergyProduction * building.productionMultiplier;
+
+            ResourceManager.Instance.AddMoney(Mathf.RoundToInt(moneyOutput));
+            ResourceManager.Instance.AddScience(Mathf.RoundToInt(scienceOutput));
+            ResourceManager.Instance.AddFood(Mathf.RoundToInt(foodOutput));
+            ResourceManager.Instance.AddEnergy(Mathf.RoundToInt(energyOutput));
+
+            Debug.Log($"[Produce] {building.buildingID} produced Money: {moneyOutput}, Science: {scienceOutput}, Food: {foodOutput}, Energy: {energyOutput}");
+        }
     }
+
+    void Start()
+    {
+        InvokeRepeating(nameof(UpdateProduction), 5f, 5f);
+    }
+
 }
